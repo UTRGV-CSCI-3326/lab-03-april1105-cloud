777bc155468f8e73a1df507cd89cb25896b49d1a
april1105-cloud <156960042+april1105-cloud@users.noreply.github.com>
2025-02-03 01:52:46 +0000

    Finished Arithmetic

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,14 @@
+public class BankAccount {
+    public static void main(String[] args){
+        double balance = 2175.37;
 
+        balance -= 302.50;
+        balance += 50.03;
+        balance = (balance/2) + 20.00;
+        balance -= 1;
+        balance *= 2;
+        balance += 1;
+
+        System.out.println("This is the new balance Bob should have: " + balance);
+    }
+}
